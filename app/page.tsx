778e018892ad621ffef5f30dd5813136e28ae0a1
Sam89-61 <samuelabdu61@gmail.com>
2026-05-08 17:07:04 +0200

    collor

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ import Player from "./components/player";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black min-h-screen">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center py-16 px-6 bg-white dark:bg-black sm:items-start transition-colors">
+    <div className="flex flex-col flex-1 items-center justify-center bg-background font-sans min-h-screen">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center py-16 px-6 bg-background sm:items-start transition-colors">
         <Pomodoro />
         <Todolist />
         <Player />
